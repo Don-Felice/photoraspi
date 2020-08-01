dc5769b25113ca8ad3a1d6712fbed78e847f0444
Don-Felice <don.felice@gmx.net>
2020-08-01 17:42:33 +0200

    Add argument parsing, untimed preview mode and series shooting.

--- photoraspi.py
+++ photoraspi.py
@@ -1,5 +1,28 @@
-from photoraspi.utils_cam import livetime
+# -*- coding: utf-8 -*-
+# FSegerer 20200801
+
+# external imports
+from picamera import PiCamera
+import argparse
+
+# project intern imports
+from photoraspi.utils_cam import livetime, shootseries
+
 
 if __name__ == "__main__":
-    # TODO
-    livetime(5, 60)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("num_imgs", type=int,
+                        help="number of images to be taken")
+    parser.add_argument("interval", type=int,
+                        help="interval between images")
+    parser.add_argument("dest_dir", type=str,
+                        help="destination directory for image output")
+    parser.add_argument("-a", "--alpha", type=int, default=60,
+                        help="transparency of preview mode")
+    args = parser.parse_args()
+
+    camera = PiCamera()
+    print("Take your time to adjust the camera, you don't wanna mess this up!!")
+    livetime(camera, alpha=args.alpha)
+    shootseries(camera, args.num_imgs, args.interval, args.dest_dir)
--- photoraspi/utils_cam.py
+++ photoraspi/utils_cam.py
@@ -1,23 +1,46 @@
+# -*- coding: utf-8 -*-
+# FSegerer 20200801
+
 from picamera import PiCamera
-from time import sleep
+import time
 from pathlib import Path
 
-def timestamp():
+
+def gettimestamp():
     localtime = time.localtime(time.time())
     timestamp = f'{localtime.tm_year}{localtime.tm_mon:02d}{localtime.tm_mday:02d}' + \
-                f'-{localtime.tm_hour:02d}{localtime.tm_min:02d}{localtime.tm_sec:02d}' #(localtime.tm_year, localtime.tm_mon, localtime.tm_mday, localtime.tm_hour, localtime.tm_min, localtime.tm_sec)
+                f'-{localtime.tm_hour:02d}{localtime.tm_min:02d}{localtime.tm_sec:02d}'
     return timestamp
 
-def livetime(timer, alpha=255):
-    camera = PiCamera()
-    camera.start_preview(alpha=alpha)
-    sleep(timer)
-    camera.stop_preview()
 
-def shootseries(num_imgs, interval, path_output):
+def livetime(camera, timer=None, alpha=255):
+    try:
+        camera.start_preview(alpha=alpha)
+        if timer:
+            time.sleep(timer)
+        else:
+            input("Press enter to continue...")
+        camera.stop_preview()
+    finally:
+        camera.stop_preview()
+
+
+def shootseries(camera, num_imgs, interval, path_output):
     if isinstance(path_output, str):
         path_output = Path(path_output)
 
+    timestamp_initial = gettimestamp()
+    path_output = path_output / f'Series_starting_{timestamp_initial}'
+
+    path_output.mkdir(parents=True, exist_ok=True)
+    print('---------------------')
+    print(f'Will start to take beautiful pictures and save them in \"{str(path_output)}\"')
+    print(f'Whole series will roughly take {interval*(num_imgs-1)/60:5.2f} minutes')
     for i in range(num_imgs):
-        camera.capture(path_output / 'image%s.jpg' % i)
-        sleep(interval)
+        timestamp = gettimestamp()
+        img_name = f'RasPic_{timestamp}.jpg'
+        camera.capture(str(path_output / img_name))
+        print(f'capturing image {i} out of {num_imgs}: {img_name}')
+        time.sleep(interval)
+    print(f'Awesome series captured and saved in \"{str(path_output)}\"')
+    print('---------------------')
